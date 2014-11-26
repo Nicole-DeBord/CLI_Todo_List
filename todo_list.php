f08280640382fa88ce8f575185895c7744784fa8
Nicole DeBord <a.nicole.debord@gmail.com>
2014-11-26 12:06:02 -0600

    Added part 1 of todo list exercise

---
+++ todo_list.php
@@ -0,0 +1,47 @@
+<?php
+
+// exercise 3.4., part 1 - update the code to allow uppercase and 
+// lowercase inputs from user - test add, remove, quit functionality
+
+// Create array to hold list of todo items
+$items = array();
+
+// The loop!
+do {
+    // Iterate through list items
+    foreach ($items as $key => $item) {
+        // Display each item and a newline
+        echo "[{$key}] {$item}\n";
+    }
+
+    // Show the menu options
+    echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+    // Get the input from user
+    // Use trim() to remove whitespace and newlines
+    $input = trim(fgets(STDIN));
+
+    // Check for actionable input
+    if ($input == 'N' || $input =='n') {
+        // Ask for entry
+        echo 'Enter item: ';
+        // Add entry to list array
+        $items[] = trim(fgets(STDIN));
+    } elseif ($input == 'R' || $input == 'r') {
+        // Remove which item?
+        echo 'Enter item number to remove: ';
+        // Get array key
+        $key = trim(fgets(STDIN));
+        // Remove from array
+        unset($items[$key]);
+    }
+// Exit when input is (Q)uit
+} while ($input != 'Q' && $input != 'q');
+
+// okay, I want to know why ^that business works - why && vs. || ?
+
+// Say Goodbye!
+echo "Goodbye!\n";
+
+// Exit with 0 errors
+exit(0);
