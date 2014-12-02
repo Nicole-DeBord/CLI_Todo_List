a049362a3cdfb2281c76a3fd4c6104a31fc411b0
Nicole DeBord <a.nicole.debord@gmail.com>
2014-12-02 13:08:46 -0600

    Added updated todo list using array sorting functions

--- todo_list.php
+++ todo_list.php
@@ -1,46 +1,34 @@
 <?php
 
-// Exercise 3.5.4. - some alternative solutions
+// Exercise 3.6.1. - Sorting arrays and other fun
 
-// Create array to hold list of todo items
+// This array will hold todo items entered by the user.
 $items = array();
 
 
+// This function returns items entered into the array as default indexed key => value list.
+// Key numbers and their values are concatenated onto an empty string so they can be echoed.
+// The key is incremented to avoid an infinite loop.
+// Our data is returned outside of the foreach loop so it will be returned after ALL iterations.
+// If $list is empty, 'no items' is returned so the users knows new items must be added.
 function listItems($list) {
-    
-    // here we set $returnedItem to an empty string....
     $returnedItem = '';
-    
     if (!(empty($list))) { 
         foreach($list as $key => $item) {
-
-            // ... so that we can concantenate this onto it:
             $returnedItem .= "[{$key}] {$item} \n";
             $key++;
         }
-            // 'return' is taken out of the foreach loop so it will
-            // capture each iteration instead of ending after the 
-            // first one
             return $returnedItem;
-
-    // an added else statement provides functionality in the 
-    // event that there is no input provided yet - this 
-    // allows the user to know what's happening 
     } else {
         return 'No items' . PHP_EOL;
     }
-        
 }
 
 // This function will get user input, and return that input.
 // We can return it with or without changing to uppercase.
 function getInput($upper = false) {
 
-
-// In the below options, I omitted '$upper == true' because it is
-// redundant - the if conditional is already checking whether or
-// or not $upper is set to true
-
+// There are a few ways to do this - 
 // option 1:
 
     if($upper) {
@@ -62,41 +50,63 @@ return ($upper) ? strtoupper($input) : $input;
     return $input;
 }
 
+// Adding a sort function.
+// Calling my getInput function to capture user input and convert it to uppercase.
+// Assigning getInput to a variable, $option, so I can pass it within my function
+function sortMenu($array, $defaultArray) {
+    echo '(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered : ';
+    $option = getInput(true);
+    if ($option == 'A') {
+        asort($array);
+    } elseif ($option == 'Z') {
+        rsort($array);
+    } elseif ($option == 'O') {
+        $array = $defaultArray;
+    } elseif ($option == 'R') {
+        krsort($defaultArray);
+        $array = $defaultArray;
+    }
+    return $array;
+}
 
 // The loop!
 do {
     // Here I call my function and my array items get listed
     echo listItems($items);
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo '(N)ew item, (R)emove item, (S)ort, (Q)uit : ';
 
     // Get the input from user
     // Use trim() to remove whitespace and newlines
     $input = getInput(true);
 
-    // Check for actionable input
+    // Check for actionable input.
+    // Ask for entry.
+    // User input is entered as values in array $items.
+    // The value of $items is assigned to $originalOrder so we can call our array values in their
+    // default order later, even if $items has been resorted.
     if ($input == 'N') {
-        // Ask for entry
         echo 'Enter item: ';
-        // Add entry to list array
         $items[] = getInput();
+        $originalOrder = $items;
+    } elseif ($input == 'S') {
+        // Calling my sort function, passing argument '$items'
+        // I've set the value of $items equal to whatever sortMenu reordered it as.
+        // When it passes back through the DO part of my loop, $items will display in their new order.
+        // $originalOrder 
+        $items = sortMenu($items, $originalOrder);
+        // Remove items.
     } elseif ($input == 'R') {
-        // Remove which item?
         echo 'Enter item number to remove: ';
-        // Get array key
-        // see how we added -1?
         $key = getInput();
-        //Make sure remove still works after setting list to start at 1
-        // Remove from array
+        // I actually have no idea why this is here. Oops.
         unset($items[$key]);
     }
-// Exit when input is (Q)uit
+// Exit when input is (Q)uit.
 } while ($input != 'Q');
-
-// okay, I want to know why ^that business works - why && vs. || ?
 
 // Say Goodbye!
 echo "Goodbye!\n";
 
-// Exit with 0 errors
+// Exit with 0 errors.
 exit(0);
